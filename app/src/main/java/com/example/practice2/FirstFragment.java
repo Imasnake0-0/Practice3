fe77968690ea61a27992b01a4586b12e0af2a6d6
KuznetsovAS <vikkyylove01@gmail.com>
2023-03-16 19:11:37 +0300

    createfragments commit

---
+++ app/src/main/java/com/example/practice2/FirstFragment.java
@@ -0,0 +1,18 @@
+package com.example.practice2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class FirstFragment extends Fragment {
+   // private Button bt1;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+}
--- app/src/main/java/com/example/practice2/MainActivity.java
+++ app/src/main/java/com/example/practice2/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.practice2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -14,15 +15,16 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private LinearLayout back;
     private Button buttonZ, buttonRas;
     private Spinner spinner;
     private static final String TAG = "Practice2";
+    private SecondFragment secondFragment = new SecondFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        back = findViewById(R.id.background);
+        setNewFragment(secondFragment);
+        //back = findViewById(R.id.background);
         buttonZ = findViewById(R.id.zapic);
         buttonRas = findViewById(R.id.raspisanie);
         spinner = (Spinner) findViewById(R.id.spinner);
@@ -41,9 +43,18 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onClickZ(View view)
     {
-        Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra("Club", (String) spinner.getSelectedItem());
-        startActivity(intent);
+        FirstFragment firstFragment = new FirstFragment();
+        setNewFragment(firstFragment);
+        //Intent intent = new Intent(this, SecondActivity.class);
+        //intent.putExtra("Club", (String) spinner.getSelectedItem());
+        //startActivity(intent);
+    }
+
+    private void setNewFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
     }
 
 
